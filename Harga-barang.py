c0a1cc2dea5a25f4e7946066d038e48ead40fdd6
Wiranti Destiani <wirantipku683@gmail.com>
2026-04-28 09:18:13 +0700

    Create Harga-barang.py
    Project ketiga saya

---
+++ Harga-barang.py
@@ -0,0 +1,17 @@
+barang="A01"
+match barang:
+  case "A01":
+    print("Nama barang:Snack")
+    print("Harga barang:10000" )
+  case "A02":
+    print("Nama barang:Minuman")
+    print("Harga barang:8000"  )
+  case "B01":
+    print("Nama barang:Mie instan")
+    print("Harga barang:3000" )
+  case "B02":
+    print("Nama barang:sabun")
+    print("Harga barang:5000" )
+  case "C01":
+    print("Nama barang:shampoo")
+    print("Harga barang:12000" )
